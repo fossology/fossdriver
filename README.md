0e38309d2db71a401d92b3c760ad3a2fcc91364d
Steve Winslow <swinslow@gmail.com>
2018-08-11 08:53:57 -0400

    Added Python packaging files
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# fossdriver
+
+fossdriver is intended to enable scriptable control of a [FOSSology server](https://www.fossology.org). (fossdriver doesn't contain a FOSSology server; you'll have to go get and set one up yourself.)
+
+This package is significantly based on, and inspired by, the excellent [fossup](https://gitlab.com/toganlabs/fossup) FOSSology Uploader package from Togán Labs.
+
+## License
+
+fossdriver is provided under the [BSD-3-Clause](https://spdx.org/licenses/BSD-3-Clause.html) license, a copy of which is included in the LICENSE file.
+
+SPDX-License-Identifier: BSD-3-Clause
---
+++ setup.py
@@ -0,0 +1,31 @@
+# Copyright The Linux Foundation
+# SPDX-License-Identifier: BSD-3-Clause
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fossdriver",
+    version="0.0.1",
+    author="Steve Winslow",
+    author_email="swinslow@gmail.com",
+    description="Python interface to control a FOSSology server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/fossdriver",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        # non-standard classifiers to designate specific license
+        "License :: OSI Approved :: BSD-3-Clause License",
+        "SPDX-License-Identifier: BSD-3-Clause",
+    ),
+    install_requires=[
+        "requests == 2.19.1",
+        "requests-toolbelt == 0.8.0",
+        "bs4 == 0.0.1",
+        "lxml == 4.2.1",
+    ]
+)
